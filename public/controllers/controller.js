0aa3a41fe2340d5c35e35b37e746df5c6c1f33ff
arcred <archith.manne@gmail.com>
2016-12-29 17:54:19 +0530

    plusminus buttons included

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,43 +1,44 @@
 var myApp=angular.module('myApp',[]);
 
 
-myApp.controller('AppCtrl',['$scope','$http',function(a,b){
+myApp.controller('itemsController',['$scope','$http',function(a,b){
+    
     console.log("Hello from controller");
     
     
+    a.searchItem=function(){
+       
+        var responsedata=[];
+        var responsedatawithcount = [];
+        console.log(a.nameItem);
+ 
+ 
+        b.post('/ecommerce',({'category':a.nameItem}))
+            .then(function(response){
+
+                console.log("Inside post from controller");
+                console.log(response);
+                console.log("AFter post in controller");
+
+                responsedata.push(response.data);
+                console.log(responsedata);
+
+                a.search=responsedata[0];     
+
+        });
+    };
     
-
-
-a.view=function(prodname){
-    var productname=[];
-productname.push(prodname);
-    console.log(productname[0].name);
-    a.name=productname[0].name;
-    a.price=productname[0].price;
-    a.category=productname[0].category;
-    a.description=productname[0].description;
-};
- 
- a.searchItem=function(){
-     var responsedata=[];
-console.log(a.nameItem);
- 
- 
-b.post('/ecommerce',({'category':a.nameItem})).then(function(response){
-    
-console.log("Inside post from controller");
-console.log(response);
-console.log("AFter post in controller");
- 
-responsedata.push(response.data);
-console.log(responsedata);
-
-a.seacrh=responsedata[0];
-
-});
-};
-    
-
+    a.view=function(prodname){
+        
+        var productname=[];
+        productname.push(prodname);
+        console.log(productname[0].name);
+        a.name=productname[0].name;
+        a.price=productname[0].price;
+        a.category=productname[0].category;
+        a.description=productname[0].description;
+        
+    };
    
 }]);
     
--- server.js
+++ server.js
@@ -6,31 +6,32 @@ var bodyParser=require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
+app.listen(3000);
+
+console.log("server running on port 3000");
  
 
 
 
 
 app.post('/ecommerce',function(req,res){
- 
-var collection=db.collection('ecommerce');
     
-console.log(req.body);
-collection.find(req.body,function(err,doc){
- 
-if(err){
-return;
-}
-else{
-res.json(doc);
-    console.log("From server.js post");
-console.log(req.body);
-}
- 
- 
+    console.log("iam in server control");
+    var collection=db.collection('ecommerce');
+
+    console.log(req.body);
+    collection.find(req.body,function(err,doc)
+    {
+
+        if(err)
+        {
+            return;
+        }
+        else
+        {
+            res.json(doc);
+            console.log("From server.js post");
+            console.log(req.body);
+        }
+    });
 });
- });
-
-app.listen(3000);
-
-console.log("server running on port 3000");
